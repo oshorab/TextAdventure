617b5853f05ab1559cc3cc943f53086fce5a2d04
oshorab971 <oshorab971@fhs.win.fuhsd.org>
2017-02-13 15:07:45 -0800

    add Entity and MovingEntity classes

---
+++ src/ca/dobervich/graph/HW5/MovingEntity.java
@@ -0,0 +1,15 @@
+package ca.dobervich.graph.HW5;
+
+import java.util.List;
+
+public abstract class MovingEntity extends Entity{
+
+	public abstract void move();
+	
+	public void moveRandom() {
+		Room neighbor = currentRoom.getRandomNeighbor();
+		// move to the neighbor
+	}
+	
+	
+}
