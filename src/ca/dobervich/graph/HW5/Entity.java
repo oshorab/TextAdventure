a354ce99d44985b3f136e8931fb1f203e31075c8
oshorab971 <oshorab971@fhs.win.fuhsd.org>
2017-02-16 14:45:38 -0800

    Entity class update

--- src/ca/dobervich/graph/HW5/Entity.java
+++ src/ca/dobervich/graph/HW5/Entity.java
@@ -2,4 +2,23 @@ package ca.dobervich.graph.HW5;
 
 public class Entity {
 
+	protected String name, description;
+	
+	public void tick();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
